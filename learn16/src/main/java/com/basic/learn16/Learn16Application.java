852ef59a1dfe3148b5b0768dc3ee46df8543e10e
zhoulibin <libin.zhou@ecloud.com.cn>
2018-11-02 18:24:41 +0800

    zhoulibin
    SpringBoot 准备 redis 缓存实验

---
+++ learn16/src/main/java/com/basic/learn16/Learn16Application.java
@@ -0,0 +1,12 @@
+package com.basic.learn16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Learn16Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Learn16Application.class, args);
+    }
+}
